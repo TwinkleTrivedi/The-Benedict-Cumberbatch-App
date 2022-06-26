f0a7af0167bcae408a1eb68d639e1f63f00b0c71
Twinkle Trivedi <34636524+TwinkleTrivedi@users.noreply.github.com>
2022-06-26 19:10:33 -0400

    movie synopsis

--- The Benedict Cumberbatch App/View/MovieDetails/MovieDetailsViewController.swift
+++ The Benedict Cumberbatch App/View/MovieDetails/MovieDetailsViewController.swift
@@ -24,7 +24,7 @@ class MovieDetailsViewController: UIViewController {
         view.addSubview(subview.overview)
         subview.MovieImage.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: subview.overviewtitle.topAnchor, right: view.rightAnchor, paddingTop:100, paddingLeft: 5, paddingBottom: 15, paddingRight: 0, width: 300, height: 300, enableInsets: false)
         subview.overviewtitle.anchor(top: nil, left: view.leftAnchor, bottom: subview.overview.topAnchor, right: view.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 5, width: 200, height: 30, enableInsets: false)
-        subview.overview.anchor(top: subview.overviewtitle.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 5, paddingLeft: 10, paddingBottom: 50, paddingRight: 5, width: 200, height: 0, enableInsets: false)
+        subview.overview.anchor(top: subview.overviewtitle.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 50, paddingRight: 5, width: 200, height: 0, enableInsets: false)
         MovieData()
     }
     
@@ -32,7 +32,7 @@ class MovieDetailsViewController: UIViewController {
         loadmovies.loadImages("\(appdelegate.imageurl)\(MovieDetails?.poster_path ?? "")") {image  in
             self.subview.MovieImage.image = image
                     }
-        subview.overviewtitle.text="Movie Overview:"
+        subview.overviewtitle.text="Movie Synopsis:"
         subview.overview.text = "\(MovieDetails?.overview ?? "")"
     }
     
